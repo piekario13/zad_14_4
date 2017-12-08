7332f5770615963b4e4bc0a9ecb89d56c73f46a7
Jacek Piekarski <32914257+piekario13@users.noreply.github.com>
2017-12-08 15:51:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -24,8 +24,24 @@ var movies = [
       img: 'https://naekranie.pl/wp-content/uploads/2014/12/hobbit-bitwa-pieciu-armii-plakat-teaser-1406807266.jpg'
     }
   ];
-  
-  var MovieTitle = React.createClass({
+
+  var Movie = React.createClass({
+    propTypes: {
+        movie: React.PropTypes.object.isRequired,
+    },
+
+    render: function() {
+        return (
+            React.createElement('li', {key: this.props.movie.id}, 
+                React.createElement(MovieTitle, {title: this.props.movie.title}),
+                React.createElement(MovieDescription, {description: this.props.movie.desc}),
+                React.createElement('img', {src: this.props.movie.img}),
+            )
+        )
+    },
+});
+
+    var MovieTitle = React.createClass({
         propTypes: {
             title: React.PropTypes.string.isRequired,
         },
@@ -47,29 +63,26 @@ var movies = [
       },
     });
 
-    var MovieImage = React.createClass({
-      propTypes: {
-          image: React.PropTypes.object.isRequired,
-      },
-      render: function() {
-          return (
-              React.createElement('img', {src: this.props.img})
-          )
-      },
-  });
+    var MoviesList = React.createClass({
+        propTypes: {
+            movies: React.PropTypes.array.isRequired,
+        },
+        render: function() {
+            return (
+                React.createElement('div', {},
+                    React.createElement('h1', {}, 'Lista filmów'),
+                    React.createElement('ul', {}, moviesElements(this.props.movies))
+                )
+            )
+        },
+    });
   
-  var moviesElements = movies.map(function(movie) {
-    return React.createElement('li', {key: movie.id},
-        React.createElement('h2', {}, movie.title),
-        React.createElement('p', {}, movie.desc),
-        React.createElement('img', {src: movie.img})
-      );
-  });
-
-  var element =
-  React.createElement('div', {},
-    React.createElement('h1', {}, 'Lista filmów'),
-    React.createElement('ul', {}, moviesElements),
-   );
-
+    var moviesElements = function(movies) {
+        return movies.map(function(movie) {
+            return React.createElement(Movie, {key: movie.id, movie: movie});
+        });
+    };
+    
+    var element = React.createElement(MoviesList, {movies: movies});
+    
     ReactDOM.render(element, document.getElementById('app'));
